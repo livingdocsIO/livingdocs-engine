5d7ac1cd1169265aa13deeb0841087f1e2696747
Gabriel <gabriel.hase@gmail.com>
2014-10-06 17:29:37 +0200

    expose more models for node_api

--- src/node_api.js
+++ src/node_api.js
@@ -1,5 +1,14 @@
 var jsdom = require("jsdom");
 global.window = jsdom.jsdom().parentWindow;
 
+$ = require('../components/jquery/jquery');
+exports.$ = $;
+
 var document = require('./document');
 exports.Document = document;
+
+var design = require('./design/design');
+exports.Design = design;
+
+var snippetTree = require('./snippet_tree/snippet_tree');
+exports.SnippetTree = snippetTree;
