b30c1d351b5e10f7c579bb56f09fb7b8bd6b2e78
Lukas <lpeyer@gmail.com>
2014-11-03 20:53:55 +0100

    Update node_api

--- src/node_api.js
+++ src/node_api.js
@@ -4,11 +4,6 @@ global.window = jsdom.jsdom().parentWindow;
 $ = require('../components/jquery/jquery');
 exports.$ = $;
 
-var document = require('./document');
-exports.Document = document;
-
-var design = require('./design/design');
-exports.Design = design;
-
-var snippetTree = require('./component_tree/component_tree');
-exports.SnippetTree = snippetTree;
+exports.Design = require('./design/design');
+exports.Livingdoc = require('./livingdoc');
+exports.ComponentTree = require('./component_tree/component_tree');
