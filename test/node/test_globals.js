5e4fd0c8b5417a4e63c3e87417f2e52263897bb0
Marc Bachmann <marc.brookman@gmail.com>
2015-01-13 14:28:20 +0100

    Add comments & update shrinkwrap file

--- test/node/test_globals.js
+++ test/node/test_globals.js
@@ -1,4 +1,3 @@
-
 // Chai
 chai = require('chai');
 expect = chai.expect;
@@ -6,11 +5,14 @@ sinon = require('sinon');
 
 
 // Extend chai with chai jquery
+// This also monkey patches the loaded jquery
 // https://github.com/chaijs/chai-jquery
 chai.use(function (_chai, utils) {
-  engine = require('../../')
-  // Patch the loaded jQuery
-  return require('../support/chai-jquery/chai-jquery')(_chai, utils, engine.jquery);
+  engine = require('../../src/node_api')
+  // This jquery module already contains a window
+  // that's set inside the node_api.coffee file
+  jquery = require('jquery')
+  return require('../support/chai-jquery/chai-jquery')(_chai, utils, jquery);
 });
 
 // Livingdocs Test Helpers
